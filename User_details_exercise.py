94c072b6ba88cdd164b92cae39e74145054a29f9
FlrMh <florina.mihai110@gmail.com>
2023-01-30 12:39:14 +0000

    User details exercise updated

---
+++ User_details_exercise.py
@@ -0,0 +1,14 @@
+current_year = 2023
+
+print("Hello, what is your name?")
+name = input()
+print(f"Hi {name}! It is great to meet you! May I ask how old are you?")
+# print(name)
+# print("How old are you?")
+age = input()
+print(f"{age}? Wow! So young!")
+print("So, what would you DOB be?")
+answer = input()
+print(f"Great! Now, just to confirm that {answer} is your DOB?")
+DOB_confirm = input(1/0)
+print("amazing! Thank you!")
